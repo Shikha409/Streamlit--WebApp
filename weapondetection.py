6603bdf8d634420f6b819b52048cb04c739820f3
Shikha409 <124184032+Shikha409@users.noreply.github.com>
2024-09-28 22:23:34 +0530

    Update weapondetection.py

--- weapondetection.py
+++ weapondetection.py
@@ -124,7 +124,7 @@ model.to(DEVICES)
 if option == "Webcam Detection":
     start_button = st.button("Start Webcam Detection")
     # Main options for camera detection modes
-     option = st.selectbox("Select Mode", ["Choose...", "Webcam Detection", "IP Camera Detection"])
+      st.selectbox("Select Mode", ["Choose...", "Webcam Detection", "IP Camera Detection"])
 
     if start_button:
         cap = cv2.VideoCapture(0)
